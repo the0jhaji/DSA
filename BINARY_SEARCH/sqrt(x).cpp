77c1e1349c6f4bbff98dcf258e25c8551fefaa1c
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-05-02 01:55:37 +0530

    sqrt(x)

---
+++ BINARY_SEARCH/sqrt(x).cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+private:
+    long long int solve(int n)
+    {
+        int s = 0, e = n;
+        long long int mid = s + (e - s) / 2;
+        long long int ans = -1;
+        while (s <= e)
+        {
+            long long int square = mid * mid;
+            if (square == n)
+            {
+                return mid;
+            }
+            if (square < n)
+            {
+                ans = mid;
+                s = mid + 1;
+            }
+            else
+            {
+                e = mid - 1;
+            }
+            mid = s + (e - s) / 2;
+        }
+        return ans;
+    }
+
+public:
+    int mySqrt(int x)
+    {
+        return solve(x);
+    }
+};
