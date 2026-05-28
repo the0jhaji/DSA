28921b137e2c8732c29ae530a94220433ae19bef
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-05-28 13:19:53 +0530

    Add implementation to validate a Binary Search Tree

---
+++ BST/day2/validBST.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+/*************************************************************
+
+    Following is the Binary Tree node structure
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+*************************************************************/
+bool solve(BinaryTreeNode<int> *root, int min, int max)
+{
+    if (root == NULL)
+    {
+        return true;
+    }
+    if (root->data > min && root->data <= max)
+    {
+        bool left = solve(root->left, min, root->data);
+        bool right = solve(root->right, root->data, max);
+        return left && right;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool validateBST(BinaryTreeNode<int> *root)
+{
+    // Write your code here
+    solve(root, INT_MIN, INT_MAX);
+}
