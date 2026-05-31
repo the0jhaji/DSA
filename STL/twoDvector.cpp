ed3698fe265a097ce007b1dade200ff91f21ae91
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-05-31 15:08:56 +0530

    Add implementation to find minimum distance between BST nodes

---
+++ STL/twoDvector.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<vector<int>> arr(5, vector<int>(5, 1));
+    //2d array created with 5 rows 4 cols with initial value of 1.
+    
+}
