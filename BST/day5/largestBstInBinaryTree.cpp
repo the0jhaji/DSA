c1fea348c456f57a60a498916958e44fa49e6786
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-05-31 12:10:55 +0530

    Add implementation to find the largest BST in a binary tree

---
+++ BST/day5/largestBstInBinaryTree.cpp
@@ -0,0 +1,49 @@
+class info
+{
+public:
+    int maxi;
+    int mini;
+    bool isBST;
+    int size;
+};
+
+info solve(TreeNode *root, int &ans)
+{
+
+    if (root == NULL)
+    {
+        return {INT_MIN, INT_MAX, true, 0};
+    }
+
+    info left = solve(root->left, ans);
+    info right = solve(root->right, ans);
+
+    info currNode;
+
+    currNode.size = left.size + right.size + 1;
+
+    currNode.maxi = max(root->data, max(left.maxi, right.maxi));
+    currNode.mini = min(root->data, min(left.mini, right.mini));
+
+    if (left.isBST && right.isBST &&
+        root->data > left.maxi &&
+        root->data < right.mini)
+    {
+
+        currNode.isBST = true;
+        ans = max(ans, currNode.size);
+    }
+    else
+    {
+        currNode.isBST = false;
+    }
+
+    return currNode;
+}
+
+int largestBST(TreeNode *root)
+{
+    int maxSize = 0;
+    solve(root, maxSize);
+    return maxSize;
+}
