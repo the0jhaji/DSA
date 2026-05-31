75a4e814940ff5739420584c4213f298fed5321d
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-05-31 13:33:26 +0530

    Refactor vector implementation to demonstrate various vector operations

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -2,9 +2,32 @@
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> mile(10);//creates an arr of size 10
-    vector<int> distance(15, 0);//create an arr of size 15 and all elements are 0.
-    for(int i=0; i<15; i++){
-        cout<< distance[i]<< " ";
-    }
+    //creation of Vector
+   vector<int> marks;
+   vector<int> miles(10);
+   vector<int> dist(10, 5);
+   marks.push_back(10);
+   marks.push_back(20);
+   marks.push_back(30);
+   marks.push_back(40);
+
+//    cout<<*marks.begin()<<endl; 
+//    cout<<*marks.end()<<endl;
+//    cout<<"Size:"<<marks.size()<<endl;    cout<<"Capacity:"<<marks.capacity()<<endl;
+//    cout<<"Max Size:"<<marks.max_size()<<endl;
+//    if(marks.empty()){
+//     cout<<"Vector is empty"<<endl;
+//    }
+//    else{
+//     cout<<"Vector is not empty"<<endl;
+//    }
+//     marks.pop_back();
+//     cout<<marks.size()<<endl;
+    marks.clear();
+    cout<<marks.size()<<endl;
+    marks.insert(marks.begin(), 10);
+    cout<<marks.size()<<endl;
+
+    return 0;
 }
+     
