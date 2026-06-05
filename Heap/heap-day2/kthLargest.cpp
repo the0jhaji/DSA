b8498c73e2b47a25f70beea47286f9c46a9a7131
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-06-05 12:12:24 +0530

    Add findKthLargest function to determine the k-th largest element in an array

---
+++ Heap/heap-day2/kthLargest.cpp
@@ -0,0 +1,23 @@
+#include <queue>
+
+class Solution
+{
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;//create a min heap 
+        for (int i = 0; i < k; i++)
+        {
+            pq.push(nums[i]);
+        }
+        for (int i = k; i < nums.size(); i++)
+        {
+            if (nums[i] > pq.top())
+            {
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        return pq.top();
+    }
+};
