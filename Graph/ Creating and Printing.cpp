1b33f110352c1ad9312c29b4c26984bd3a2159d3
the0jhaji <adarshkumarojha.aiml2024@nsec.ac.in>
2026-06-14 13:49:08 +0530

    Add function to print adjacency list representation of a graph

---
+++ Graph/ Creating and Printing.cpp
@@ -0,0 +1,29 @@
+vector<vector<int>> printAdjacency(int n, int m, vector<vector<int>> &edges)
+{
+    vector<vector<int>> ans(n);
+
+    // Build adjacency list
+    for (int i = 0; i < m; i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        ans[u].push_back(v);
+        ans[v].push_back(u);
+    }
+
+    vector<vector<int>> adj(n);
+
+    // Prepare required output
+    for (int i = 0; i < n; i++)
+    {
+        adj[i].push_back(i);
+
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            adj[i].push_back(ans[i][j]);
+        }
+    }
+
+    return adj;
+}
