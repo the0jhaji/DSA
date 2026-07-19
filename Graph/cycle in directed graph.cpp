52ea15408fcec79832bf8df42859f4fea932a648
Adarsh Kumar Ojha <ojhaadarsh424@gmail.com>
2026-07-19 08:19:57 +0530

    Implement cycle detection in directed graph

---
+++ Graph/cycle in directed graph.cpp
@@ -0,0 +1,59 @@
+#include <unordered_map>
+#include <list>
+#include <vector>
+using namespace std;
+
+bool checkCycleDFS(int node,
+                   unordered_map<int,bool> &visited,
+                   unordered_map<int,bool> &dfsVisited,
+                   unordered_map<int,list<int>> &adj)
+{
+    visited[node] = true;
+    dfsVisited[node] = true;
+
+    for(auto neighbour : adj[node])
+    {
+        if(!visited[neighbour])
+        {
+            bool cycleDetected =
+                checkCycleDFS(neighbour, visited, dfsVisited, adj);
+
+            if(cycleDetected)
+                return true;
+        }
+        else if(dfsVisited[neighbour])
+        {
+            return true;
+        }
+    }
+
+    dfsVisited[node] = false;
+    return false;
+}
+
+int detectCycleInDirectedGraph(int n, vector<pair<int,int>> &edges)
+{
+    unordered_map<int,list<int>> adj;
+
+    for(int i = 0; i < edges.size(); i++)
+    {
+        int u = edges[i].first;
+        int v = edges[i].second;
+
+        adj[u].push_back(v);
+    }
+
+    unordered_map<int,bool> visited;
+    unordered_map<int,bool> dfsVisited;
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(!visited[i])
+        {
+            if(checkCycleDFS(i, visited, dfsVisited, adj))
+                return true;
+        }
+    }
+
+    return false;
+}
